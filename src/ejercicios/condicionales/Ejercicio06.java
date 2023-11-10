8a2f9fb6dac0612eec922fd4ec5e9c7fb14d3035
hectoraria <145675493+hectoraria@users.noreply.github.com>
2023-11-10 14:40:36 +0100

    wedwdw

--- src/ejercicios/condicionales/Ejercicio06.java
+++ src/ejercicios/condicionales/Ejercicio06.java
@@ -1,9 +1,24 @@
 package ejercicios.condicionales;
 
+import java.util.Random;
+import java.util.Scanner;
 public class Ejercicio06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Variable de dos numeros generados por la maquina
+		int num1,num2;
+		
+		//Inicio escaner
+		Scanner sc = new Scanner(System.in);
+		
+		// Ininicio un ramdom y le doy un ramdom a los dos numeros
+		Random r = new Random();
+		// Random n1
+		num1 = r.nextInt(1,100);
+		// Random n2
+		num2 = r.nextInt(1,100);
+		
+		
 
 	}
 
